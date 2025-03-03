2dc3f3f40b81718acbf0ba2344621d7735c8da68
NitinTheGreat <nitinpandey1304@gmail.com>
2025-03-04 00:01:47 +0530

    initial: tailwind setup

---
+++ web-app/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='bg-gray-100 text-red-500 p-4'>
+        hello
+      </div>
+    </>
+  )
+}
+
+export default App
