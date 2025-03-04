04ffc8106cffa23f4b9ce7c2a9beda47e9ee662a
NitinTheGreat <nitinpandey1304@gmail.com>
2025-03-05 01:50:44 +0530

    chore: changes to WTsection

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,18 +1,22 @@
 
 import './App.css'
 import FaqSection from "../components/FaqSection"
-import  ScrollAnimationSection  from "../components/AboutWT"
+import HorizontalRevealSection from '../components/AboutWT'
 import Dummy from '../components/dummy'
 function App() {
   return (
-    <>
+    
+    <div className="container bg-background">
       <div className=' text-red-500 bg-background font-heading'>
         Blah blah blah
       </div>
       <FaqSection />
-      <ScrollAnimationSection id="scroll-animation-section" />
+      {/* <ScrollAnimationSection id="scroll-animation-section" />
+       */}
+       <HorizontalRevealSection id="animation-section" />
       <Dummy />
-    </>
+      </div>
+    
   )
 }
 
