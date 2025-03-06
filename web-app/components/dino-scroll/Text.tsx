989a76b78b39b8b7c86a51f0d8d415c438a68bea
raisaaajose <168289547+raisaaajose@users.noreply.github.com>
2025-03-07 02:53:46 +0530

    added dino scrolling on bike

---
+++ web-app/components/dino-scroll/Text.tsx
@@ -0,0 +1,13 @@
+import '../../public/dino-bike/text.png';
+import './Text.css';
+import Thugdino from "./thugdino";
+
+function Text() {
+  return (
+    <div className="full-container">
+        <div className="scroll-text"></div>
+    </div>
+  );
+}
+
+export default Text;
